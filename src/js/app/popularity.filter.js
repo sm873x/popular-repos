d9774876dfddb8717f1472faf4c33065a7cd8d04
Stella Ma <sm873x@gmail.com>
2016-08-16 22:22:22 -0400

    Fixed popularity sort function to successfully display in descending order repos by popularity number. If repo popularity is the same, then repos are displayed by most recent date.

---
+++ src/js/app/popularity.filter.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular.module('pop')
+        .filter('popularity', PopularityFilter);
+
+    function PopularityFilter() {
+        return function popularity(repos, byDate) {
+            var newRepos;
+
+            newRepos = repos.sort(function popular(repo1, repo2) {
+                return (repo1.popularity - repo2.popularity) * -1;
+            }) ;
+
+            if (byDate) {
+                newRepos= newRepos.sort(function (repo1, repo2) {
+                    if (repo1.popularity === repo2.popularity) {
+                        return (new Date(repo1.created_at) - new Date(repo2.created_at)) * -1;
+                    }
+                });
+            }
+
+            return newRepos;
+        };
+    }
+})();
